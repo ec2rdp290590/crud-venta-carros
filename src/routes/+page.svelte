78c3d3707a526181f94927f42047bc01777a95eb
ec2rdp290590 <ec2rdp290590@gmail.com>
2025-03-23 15:32:37 -0400

    Create +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import CarList from '$lib/components/CarList.svelte';
+</script>
+
+<svelte:head>
+  <title>Concesionario de Vehículos - Gestión de Inventario</title>
+  <meta name="description" content="Sistema de gestión de inventario para concesionario de vehículos" />
+</svelte:head>
+
+<main>
+  <CarList />
+</main>
